c55d61ecacaf13ac77a2fe815ee4167b07f8d58f
drummerrev <40744479+drummerrev@users.noreply.github.com>
2018-07-08 19:11:16 -0500

    Update DisableExpiredAccounts2.ps1

--- DisableExpiredAccounts2.ps1
+++ DisableExpiredAccounts2.ps1
@@ -3,10 +3,10 @@ Import-Module ActiveDirectory -Verbose:$false
  
 #region: Define variables
 #Get expired AD accounts that are not disabled
-$ExpiredAccountsNotDisabled = Search-ADAccount -AccountExpired -Server:"Nimbus-AD01.corp.warbyparker.com" | Where-Object { $_.Enabled -eq $true }
+$ExpiredAccountsNotDisabled = Search-ADAccount -AccountExpired -Server:"server.name" | Where-Object { $_.Enabled -eq $true }
  
 #Define OU for Disabled Accounts. Change to reflect your OU
-$DisabledAccountsOU = 'OU=Disabled Users,DC=corp,DC=warbyparker,DC=com'
+$DisabledAccountsOU = 'OU=Disabled Users,DC=,DC=,DC=com'
  
 #Get current date
 $Date = Get-Date -Format g
@@ -15,10 +15,10 @@ $Date = Get-Date -Format g
 #Email Expired Accounts List to AD Account Auditors
 Search-ADAccount -AccountExpired | Where-Object { $_.Enabled -eq $true } | Export-Csv "C:\WP_Scripts\ExpiredandDisabledToday.csv" -encoding "unicode"
 
-Send-MailMessage -To "jeremy.long <jeremy.long@warbyparker.com>" -Cc "jeremy.long <jeremy.long@warbyparker.com>" -From "WP AD Reporting <tech@warbyparker.com>" `
+Send-MailMessage -To "username <email address>" -Cc "username <email address>" -From "WP AD Reporting <email address>" `
  -Subject "Expired AD Accounts that are Now Disabled" `
  -Body "The attached CSV file contains the Warby Parker Active Directory accounts that expired today and were disabled by Daily Automation." `
- -Attachments "c:\WP_Scripts\ExpiredandDisabledToday.csv" -SmtpServer smtp.warby.io
+ -Attachments "c:\WP_Scripts\ExpiredandDisabledToday.csv" -SmtpServer smtp.server.name
 
  
 #Loop through Expired Users
