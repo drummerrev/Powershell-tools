a25cd6466293b88448eef166a3799e3970e1f86b
drummerrev <40744479+drummerrev@users.noreply.github.com>
2018-07-08 19:08:47 -0500

    Update AccountsCreatedinLastXDays.ps1

--- AccountsCreatedinLastXDays.ps1
+++ AccountsCreatedinLastXDays.ps1
@@ -5,12 +5,12 @@ $When = ((Get-Date).AddDays(-7)).Date
 Get-ADUser -Property Name,mail,whenCreated,LastLogonDate,Modified,DistinguishedName -Filter {whenCreated -ge $When} |
 sort whenCreated | Select -Property Name,mail,whenCreated,LastLogonDate,Modified,DistinguishedName | 
 # out-gridview -title "Recently Created Accounts" <--- for debugging and stdout results display
-Export-Csv "C:\Users\jeremy.long\RecentlyCreatedAccounts.csv" -encoding "unicode"
+Export-Csv "C:\Users\username\RecentlyCreatedAccounts.csv" -encoding "unicode"
 
 #Sends Email of results to designated Account auditors
 
-Send-MailMessage -To "jeremy.long <jeremy.long@warbyparker.com>" -Cc "jon.mann <jon.mann@warbyparker.com>" -From "WP AD Reporting <tech@warbyparker.com>" `
+Send-MailMessage -To "username <email address>" -Cc "username <jemail address>" -From "WP AD Reporting <email address>" `
  -Subject "AD Accounts Created in Last Week" `
  -Body "The attached CSV file contains the Warby Parker Active Directory accounts that were created within the last 7 days.  
       The report includes last Modification date and last Logon date as well for account auditing purposes." `
- -Attachments "c:\Users\jeremy.long\RecentlyCreatedAccounts.csv" -SmtpServer smtp.warby.io
+ -Attachments "c:\Users\username\RecentlyCreatedAccounts.csv" -SmtpServer smtp.server.name
