1a5f11c00c25c4a5e2ee7cf3d31a3af0be7b9a37
drummerrev <40744479+drummerrev@users.noreply.github.com>
2018-07-08 19:18:19 -0500

    Update retailjiracleanup.ps1

--- retailjiracleanup.ps1
+++ retailjiracleanup.ps1
@@ -1,5 +1,5 @@
 ﻿#Sript Collects all members of DUO_Enable, filtered to Retail Users, then iterates through list and removes from DUO_Enable AD Group
 
-Get-ADGroupMember jira-cx | Where-Object -FilterScript { $_.distinguishedName -like '*OU=WPRetail*' } |
+Get-ADGroupMember jira-cx | Where-Object -FilterScript { $_.distinguishedName -like '*OU=OUNAME*' } |
  ForEach-Object -Process {
   Remove-ADPrincipalGroupMembership -Identity $_ -MemberOf jira-cx -Confirm:$false }
