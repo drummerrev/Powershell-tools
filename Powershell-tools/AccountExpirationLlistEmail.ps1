10b35ed6e05f3fe31f6d6b6ff2d1db3060ee02fd
drummerrev <40744479+drummerrev@users.noreply.github.com>
2018-09-12 09:31:28 -0500

    Delete AccountExpirationLlistEmail.ps1

--- Powershell-tools/AccountExpirationLlistEmail.ps1
+++
@@ -1,8 +0,0 @@
-Search-ADAccount -AccountExpiring -TimeSpan "10" | 
-Export-Csv "C:\Users\jeremy.long\AccountExpiringwithin10Days.csv" -encoding "unicode"
-Send-MailMessage -To "jeremy.long <jeremy.long@warbyparker.com>" -Cc "jeremy.long <jeremy.long@warbyparker.com>" -From "WP AD Reporting <tech@warbyparker.com>" `
- -Subject "Expiring Accounts" `
- -Body "The attached CSV file contains the Warby Parker Active Directory accounts that are set to expire within 10 Days.  
-      These are Temp Hire/Intern accounts that will be automatically disabled in AD once the Expiry date has been reached.  
-	  Please reach out to acting Managers to verify account status." `
- -Attachments "c:\Users\jeremy.long\AccountExpiringwithin10Days.csv" -SmtpServer smtp.warby.io
