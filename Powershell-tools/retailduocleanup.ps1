3c49f76828f90387c568e3ccd2b38c8f0468b4e0
drummerrev <40744479+drummerrev@users.noreply.github.com>
2018-09-12 09:45:07 -0500

    Delete retailduocleanup.ps1

--- Powershell-tools/retailduocleanup.ps1
+++
@@ -1,5 +0,0 @@
-﻿#Sript Collects all members of DUO_Enable, filtered to Retail Users, then iterates through list and removes from DUO_Enable AD Group
-
-Get-ADGroupMember DUO_Enable | Where-Object -FilterScript { $_.distinguishedName -like '*OU=WPRetail*' } |
- ForEach-Object -Process {
-  Remove-ADPrincipalGroupMembership -Identity $_ -MemberOf DUO_Enable -Confirm:$false }
